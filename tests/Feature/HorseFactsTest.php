f79ca99fac62ee8d79a190bd112408ab4e9adcb5
Jack Naughton <john.naughton@KOG-C02XR0DJJG5M.local>
2019-12-21 14:21:21 -0500

    api endpoint created

--- tests/Feature/HorseFactsTest.php
+++ tests/Feature/HorseFactsTest.php
@@ -8,12 +8,14 @@ use Tests\TestCase;
 
 class HorseFactsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
     public function a_post_request_can_be_sent_to_the_horsefacts_api_endpoint()
     {
-        $response = $this->post('/horsefact');
+        $response = $this->post('/api/horsefact');
 
         $response->assertStatus(200);
     }
