7128f95f1ba765d3d77e52f647cc30c8d1f3d95e
Jack Naughton <home@Jacks-MBP.verizon.net>
2020-12-10 16:37:35 -0500

    initial commit. contains code to return random horsefacts

---
+++ horsefacts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HorsefactsConfig(AppConfig):
+    name = 'horsefacts'
---
+++ horsefacts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import HorseFact
+
+# Create your views here.
+
+def random(request):
+	# lots of people claiming this is inefficient, but we won't have many facts (<200)
+    fact = HorseFact.objects.order_by('?').first()
+
+    return HttpResponse(fact)
